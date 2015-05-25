687ade8bfd86d9c1d2d7d1f270399f98873bc436
sunsongtao <sunsongtao@easyway.net.cn>
2015-05-25 09:15:13 +0800

    new minorId

--- src/controllers/pa.js
+++ src/controllers/pa.js
@@ -29,6 +29,8 @@ module.exports = function(router){
             case 40928:page.Result = 3;break;//AB:70223D     //DID 680407     //
 
             case 103:page.Result = 1;break;//AB:6E4EB4       //DID 698825     //
+            case 36957:page.Result = 1;break;
+
 
             case 104:page.Result = 2;break;//AB:7018D3                //苏州李军
 
